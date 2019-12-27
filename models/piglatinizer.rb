b29dca98b3e9d0f1763ca76fc7513b98bee77cf0
Oana Berentan <oana.berentan@yahoo.com>
2019-12-27 18:19:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,20 @@ class PigLatinizer
   attr_reader :word
 
   def piglatinize(word)
+    vowels = %w{a e i o u}
+  phrase = string.split(" ")
+  phrase.map! do |word|
+    letters = word.split("")
+    find_vowel = letters.index do |letter|
+      vowels.include?(letter)
+    end
+    #turn "square" into "aresquay"
+    if letters[find_vowel] == "u"
+      find_vowel += 1
+    end
+    letters.rotate!(find_vowel)
+    letters.push("ay")
+    letters.join
   end
 
 end
